f8694b4d248ac9af871cdf1b55ef6f1dcbabc0c0
jk090311 <110903kirby@gmail.com>
2025-05-27 20:11:45 +0800

    bgmusic added

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip MainMenu;
+
+    private void Start()
+    {
+        musicSource.clip = MainMenu;
+        musicSource.Play();
+    }
+}
